f6131f3e2fcc829dbb09adf99985cf6e4c478a79
Pronina Alyona <pronina.alyona@list.ru>
2022-11-15 22:25:54 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int priceTicket = 328;
+        int oneMile = 20;
+        int bonusMiles = priceTicket / oneMile;
+        System.out.println("Количество бонусных миль = " + bonusMiles);
+    }
+}
